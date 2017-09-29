3c243d7e6a3dd9ec5f4c4322b71273f3d75eb1ae
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-28 20:56:25 -0400

    Create instance classes to harvest fields

--- farm.rb
+++ farm.rb
@@ -33,9 +33,10 @@ class Field
         user_area = gets.chomp.to_i
         Field.plant_wheat(user_area)
       end
+      puts "Added a #{user_crop} field of #{user_area} hectares!"
     when "harvest" then
     when "status" then status
-    when "relax" then
+    when "relax" then relax
     when "exit" then abort
     end
   end
@@ -52,33 +53,56 @@ class Field
     puts "The farm has harvested #{@@harvested} food so far."
   end
 
-  def rest
-    puts "#{Corn.area} hectares of tall green stalks rustling in the breeze fill your horizon. \nThe sun hangs low, casting an orange glow on a sea of #{Wheat.area} hectares of wheat"
+  def relax
+  if Corn.planted_area > 0
+    puts "#{Corn.planted_area} hectares of tall green stalks rustling in the breeze fill your horizon."
+  end
+  if Wheat.planted_area > 0
+    puts "The sun hangs low, casting an orange glow on a sea of #{Wheat.planted_area} hectares of wheat"
+  end
+  if Corn.planted_area > 0 && Wheat.planted_area > 0
+    puts "You gaze over a barren landscape and hope for all that could be."
+  end
   end
 
 end
 
 class Corn < Field
   @@corn = 0
+  @@planted_area = 0
   def initialize(area)
     @area = area
     @corn = area * 20
+    @@planted_area += area
   end
 
-  def area
-    @area
+  def self.planted_area
+    @@planted_area
+  end
+
+  def self.harvest
+    @@corn += @corn
+    @@planted_area = 0
   end
 end
 
 class Wheat < Field
+  @@wheat = 0
+  @@planted_area = 0
   def initialize(area)
     @area = area
     @wheat = area * 30
   end
 
-  def area
-    @area
+  def self.planted_area
+    @@planted_area
   end
+
+  def self.harvest
+    @@wheat += @wheat
+    @@planted_area = 0
+  end
+
 end
 
 farm = Field.new
